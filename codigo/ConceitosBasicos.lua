396cf092f9768516e38dddc44172449c30db7a95
mario <sergio.mario_q@hotmail.com>
2013-10-08 11:53:45 -0300

    Modificações na estrutura da apresentação e na documentação final.

--- codigo/ConceitosBasicos.lua
+++ codigo/ConceitosBasicos.lua
@@ -4,7 +4,7 @@
 local tabela = {} -- O programa pode ter variáveis locais e globais, para ser global é só escrever a variável sem o nome 'local'
 local tabela2 = {1,2,3}
 
--- Lua é uma linguame que possui o modelo de declaração implícita e vinvulação dinâmica
+-- Lua é uma linguagem que possui o modelo de declaração implícita e vinvulação dinâmica
 -- AS TABELAS PODEM TER EM SEUS ÍNDICES DIFERENTES TIPOS DE DADOS, CONFORME ABAIXO:
 tabela[1] = "paradigmas" -- STRING
 tabela[2] = true -- BOOLEANO
@@ -13,12 +13,12 @@ tabela[4] = 10.55 -- REAL
 tabela[5] = tabela2 -- OUTRA TABELA
 tabela["sequencia"] = "seq" -- O INDICE TAMBÉM PODE SER OUTRO TIPO DE DADO, E RECEBER UM VALOR QUAlQUER
 
-print(tabela.sequencia)
-
 
 for i,v in pairs(tabela) do
 	print(i,v)
 end
+
+print("\n")
 
 for i,v in pairs(tabela2) do
 	print(i,v)
